5ff1bf8db35429fa70cd628c44e7ce4de105fb6e
DoctorSalt <joseamunozp@gmail.com>
2019-05-31 19:50:22 +0200

    Aplicado el JavadDoc al proyecto

--- Practica6/src/practica6/Coche.java
+++ Practica6/src/practica6/Coche.java
@@ -1,15 +1,28 @@
 package practica6;
 //version guardada
+/**
+ * 
+ * @author MOSQUETEX
+ * @version UltimaSupongo
+ *
+ */
 public class Coche {
 	public static final String EL_COCHE_CON_MATRICULA = "El coche con matricula ";
 	CocheData data = new CocheData();
-
+	
 	public Coche() {
 		data.setMatricula("");
 		data.setCombustible("");
 		data.setModelo("");
 		data.setFabricante("");
 	}
+	/**
+	 * 
+	 * @param m este parametro es la matricula del coche
+	 * @param c este parametro es el combustible del coche
+	 * @param mo este parametro es el modelo del coche
+	 * @param f este parametro es el fabricante del coche
+	 */
 
 	public Coche(String m, String c, String mo, String f) {
 		data.setMatricula(m);
@@ -18,6 +31,10 @@ public class Coche {
 		data.setFabricante(f);
 	}
 
+	/**
+	 * 
+	 * @return Devuelve el string que define el coche recogido
+	 */
 	public String metodo1() {
 		String resultado = "";
 
@@ -38,17 +55,35 @@ public class Coche {
 		return definicion_Coche;
 	}
 
-	private String metodoA(String modelo, String fabricante) {
+	/**
+	 * 
+	 * @param modelo Este es el modelo recogido del actual coche
+	 * @param fabricante Este es el fabricante recogido del actual coche
+	 * @return Devuelve "es un @param modelo @param fabricante  y gasta 1,337 euros por litro."
+	 */
+	public String metodoA(String modelo, String fabricante) {
 		String devuelve ="es un "+modelo+" "+fabricante+" y gasta 1,337 por litro.";
 		return devuelve;
 	}
 
-	private String metodoB(String modelo, String fabricante) {
+	/**
+	 * 
+	 * @param modelo Este es el modelo recogido del actual coche
+	 * @param fabricante Este es el fabricante recogido del actual coche
+	 * @return Devuelve "es un @param modelo @param fabricante  y gasta 1,052 euros por litro."
+	 */
+	public String metodoB(String modelo, String fabricante) {
 		String devuelve ="es un "+modelo+" "+fabricante+" y gasta 1,052 por litro.";
 		return devuelve;
 	}
 
-	private String metodoC(String modelo, String fabricante) {
+	/**
+	 * 
+	 * @param modelo Este es el modelo recogido del actual coche
+	 * @param fabricante Este es el fabricante recogido del actual coche
+	 * @return Devuelve "es un @param modelo @param fabricante  y no necesita combustible."
+	 */
+	public String metodoC(String modelo, String fabricante) {
 		String devuelve ="es un "+modelo+" "+fabricante+" y no necesita combustible.";
 		return devuelve;
 	}
--- Practica6/src/practica6/Moto.java
+++ Practica6/src/practica6/Moto.java
@@ -9,6 +9,11 @@ public class Moto {
 		data.setCilindrada(0);
 	}
 
+	/**
+	 * 
+	 * @param m Esta es la matricula de la moto
+	 * @param c Esta es la cilindrada de la moto
+	 */
 	public Moto(String m, int c) {
 		data.setMatricula(m);
 		data.setCilindrada(c);
